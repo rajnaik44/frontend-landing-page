1d18d8006c1b7aaa7f705e9dbf98d11c44950305
Raj <naik>
2024-07-13 05:54:26 -0700

    build error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
+
 import Hero from "./components/Hero";
-import { Main } from "next/document";
 import Features from "./components/Features";
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
       <Hero />
       <Features />
     
